b48ac3806f979c48cef0b4124619a260cf69fdcb
gitit4321 <gitit4321@gmail.com>
2021-07-28 21:12:48 -0400

    Added user authentication and profile image

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,8 +1,13 @@
 from datetime import datetime
-from todo_app import db
+from todo_app import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -11,7 +16,7 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', {self.email}', {self.image_file}')"
+        return f"User('{self.username}', {self.email}, {self.image_file})"
     
 
 class Post(db.Model):
--- todo_app/routes.py
+++ todo_app/routes.py
@@ -1,20 +1,27 @@
-from flask import render_template, url_for, flash, redirect
-from todo_app import app
-from todo_app.forms import RegistrationForm, LoginForm
+import os
+import secrets
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request
+from todo_app import app, db, bcrypt
+from todo_app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from todo_app.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 posts = [
     {
-        'author': "PDUB",
-        'title': "Blog Post 1",
-        'content': 'first post content',
+        'content': 'Wash the Car',
         'date_posted': 'July 20, 2021'
     },
     {
-        'author': "Speno",
-        'title': "Blog Post 2",
-        'content': 'second post content',
+        'content': 'Do the Dishes',
+        'date_posted': 'July 20, 2021'
+    }
+]
+
+completeds = [
+    {
+        'content': 'Buy Beer',
         'date_posted': 'July 21, 2021'
     }
 ]
@@ -22,7 +29,7 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", posts=posts)
+    return render_template("home.html", posts=posts, completeds=completeds)
 
 
 @app.route("/about")
@@ -32,20 +39,78 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been successfully created.', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@todo.com' and form.password.data == 'password':
-            flash('You have been logged in', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login Unsucessful. Please check username and password.', 'danger')
+            flash('Login Unsuccessful. Verify that you entered the correct email and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been successfully updated.', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+# @app.route("/post/new", methods=['GET', 'POST'])
+# @login_required
+# def new_post():
+#     form = PostForm()
+#     if form.validate_on_submit():
+#         flash('You added a new To Do item!', 'success')
+#         return 
+#     return render_template('create_post.html', title='New Post', form=form)
